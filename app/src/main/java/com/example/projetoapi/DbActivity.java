6bc88c7364d7167d5e68d24b658f7385083bd739
tamaxx <pedro.tamas@uol.com.br>
2021-03-26 21:29:31 -0300

    definitive update

--- app/src/main/java/com/example/projetoapi/DbActivity.java
+++ app/src/main/java/com/example/projetoapi/DbActivity.java
@@ -37,7 +37,9 @@ public class DbActivity extends AppCompatActivity {
         Cursor cursor = crud.carrega();
 
         while(cursor.moveToNext()){
-            sliderItems.add(new SliderItem(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6), cursor.getString(7)));
+            sliderItems.add(new SliderItem(cursor.getString(0), cursor.getString(1),
+                    cursor.getString(2), cursor.getString(3), cursor.getString(4),
+                    cursor.getString(5), cursor.getString(6), cursor.getString(7)));
         }
 
         viewPager2.setAdapter(new com.example.projetoapi.SliderAdapter(sliderItems, viewPager2));
